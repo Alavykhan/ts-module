3d30847c32e2aba97e01ed8f76e456231297f717
Alavykhan <alvykhan@gmail.com>
2024-11-06 12:05:21 +0600

    class 1 close

--- src/3.1.ts
+++ src/3.1.ts
@@ -1,6 +1,34 @@
 {
     //
-    
+    // opp class
 
+    class Animal {
+    //    public name: string;
+    //    public species: string;
+    //    public size: number;
+
+
+        // parameter properties
+
+        constructor(public name:string, public species: string, public size: number){
+            // this.name= name;
+            // this.species= species;
+            // this.size= size
+        }
+
+
+        showingExistence(){
+            console.log(`The animal ${this.name} belongs from ${this.species} family`)
+        }
+
+
+    }
+
+    
+    const dogi= new Animal('german shaperd', 'dog', 20)
+
+    const snake= new Animal ('king cobra', 'snake', 10)
+        dogi.showingExistence()   
+        snake
     //
 }
